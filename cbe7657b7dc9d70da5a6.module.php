fb6759887074392cf77c6d845018362b0b4ef596
ProgramistaZpolski <piotrbadelek@protonmail.com>
2021-01-26 14:38:36 +0100

    Update cbe7657b7dc9d70da5a6.module.php

--- cbe7657b7dc9d70da5a6.module.php
+++ cbe7657b7dc9d70da5a6.module.php
@@ -1,6 +1,6 @@
 <?php
-// czemu?
-// ruffle wymaga content-type jako application/wasm
+// why?
+// ruffle requires content-type as application/wasm
 // enjoy this awesome proxy ~pzpl
 header('Content-type: application/wasm');
 echo file_get_contents("cbe7657b7dc9d70da5a6.module.wasm");
